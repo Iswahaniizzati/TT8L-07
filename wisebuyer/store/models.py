6ac6edcedece19f1ee746de3511dc506c2d4b759
Nur Iswahani <iswahani.izzati@gmail.com>
2024-05-15 19:38:43 +0800

    Fixed typo in django models

--- wisebuyer/store/models.py
+++ wisebuyer/store/models.py
@@ -39,8 +39,8 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer =models.ForeignKey(Customer, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-    address = models.CharField(max_Length=100, default='',blank=True)
-    phone = models.CharField(max_Length=20,default='',blank=True)
+    address = models.CharField(max_length=100, default='',blank=True)
+    phone = models.CharField(max_length=20,default='',blank=True)
     date = models.DateField(default=datetime.datetime.today)
     status = models.BooleanFieald(default=False)
 
