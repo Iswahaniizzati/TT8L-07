2276718fdb7df909dd700477aea5b15098203d82
Kyra <kyraafia.fci@gmail.com>
2024-05-15 17:13:55 +0800

    Fixed indentation and other errors

--- wisebuyer/payment/models.py
+++ wisebuyer/payment/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import User
 
 
 class ShippingAddress(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-	full_name = models.CharField(max_length=255)
-	email = models.CharField(max_length=255)
-	address1 = models.CharField(max_length=255)
-	address2 = models.CharField(max_length=255)
-	postcode = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
     
     #Don't pluralize address
     class Meta:
-        verbosa_name_plural = "Shipping Address"
+        verbose_name_plural = "Shipping Address"
         
     def __str__(self):
         return f'Shipping Address - {str(self.id)}'
