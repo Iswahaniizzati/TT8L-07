e85a1d7d6d0f0a0f6565866408726db21ccdf287
Kyra <kyraafia.fci@gmail.com>
2024-05-26 22:11:39 +0800

    Create order model

--- wisebuyer/store/urls.py
+++ wisebuyer/store/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='home'),
-    path('update_user/', view.update_user, name='update_user'),
+    path('update_user/', views.update_user, name='update_user'),
 ]
