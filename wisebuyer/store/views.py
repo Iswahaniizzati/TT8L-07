98324c9b3d1dcbecccf147f3ed913a9a91ae29b8
Nooratierah <nooratierahsyafiqah@gmail.com>
2024-05-28 04:21:17 +0800

    created category view

--- wisebuyer/store/views.py
+++ wisebuyer/store/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Categories
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
+
+
+
+
+
+def category(request,foo):
+    #Replace hyphens with spaces
+    foo = foo.replace('-',' ')
+    #Grab the category from the url
+    try:
+        #Look up the category
+        category = Categories.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+    except:
+     messages.success(request,("Sorry, that category doesn't exist"))
+     return redirect('home')
+
+def product(request,pk):
+     product = Product.objects.get(id=pk)
+     return render(request, 'product.html', {'product': product})
 
 
 
@@ -10,7 +32,8 @@ def update_user(request):
 
 def home(request):
     products = Product.objects.all()
-    return render(request, 'home.html', {'products': products})
+    categories = Categories.objects.all()
+    return render(request, 'home.html', {'products': products, 'categories': categories})
 
 def about(request):
     return render(request, 'about.html', {})
