e100bd52c040e567e582152a4f9a79fe496b43d5
Kyra <kyraafia.fci@gmail.com>
2024-05-27 23:13:22 +0800

    Create checkout view

--- wisebuyer/payment/views.py
+++ wisebuyer/payment/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-def payment_success(request):
 
+
+def checkout(request):
+    return render(request, "payment/checkout.html", {})
+
+
+def payment_success(request):
     return render(request, "payment/payment_success.html", {})
