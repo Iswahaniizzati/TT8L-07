c8f52adea3207432457966daaf467a61efa8ff1a
Nur Iswahani <iswahani.izzati@gmail.com>
2024-05-27 23:31:54 +0800

    template out website and created about page

--- wisebuyer/store/urls.py
+++ wisebuyer/store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home,name='home'),
     path('update_user/', views.update_user, name='update_user'),
+     path('about/', views.about, name='about'),
 ]
--- wisebuyer/store/views.py
+++ wisebuyer/store/views.py
@@ -7,3 +7,6 @@ def update_user(request):
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
+
+def about(request):
+    return render(request, 'about.html', {})
