3714b5f18dd8bd7667bfb206481f1c1dff73802c
Kyra <kyraafia.fci@gmail.com>
2024-05-28 12:23:35 +0800

    import user model

--- wisebuyer/store/models.py
+++ wisebuyer/store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Categories(models.Model):
     name = models.CharField(max_length=50)
