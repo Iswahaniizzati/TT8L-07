64b6a873435bab886d7581133e457c8185de712f
Kyra <kyraafia.fci@gmail.com>
2024-06-04 23:54:32 +0800

    import signals to payment models

--- wisebuyer/payment/models.py
+++ wisebuyer/payment/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     shipping_full_name = models.CharField(max_length=255)
     shipping_email = models.CharField(max_length=255)
     shipping_address1 = models.CharField(max_length=255)
-    shipping_address2 = models.CharField(max_length=255)
+    shipping_address2 = models.CharField(max_length=255, null=True, blank=True)
     shipping_postcode = models.CharField(max_length=255)
     shipping_city = models.CharField(max_length=255)
     shipping_state = models.CharField(max_length=255)
