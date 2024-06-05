bc9136cc58ac233e1aa46498d2c49f74623f1fc3
Kyra <kyraafia.fci@gmail.com>
2024-06-05 10:48:41 +0800

    get order summary data

--- wisebuyer/payment/views.py
+++ wisebuyer/payment/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from cart.cart import Cart
 
 
 def checkout(request):
-    return render(request, "payment/checkout.html", {})
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    return render (request, "payment/checkout.html", {"cart_products":cart_products}) #add ", "quantities":quantities, "totals":totals" after variable is defined
+
 
 
 def payment_success(request):
