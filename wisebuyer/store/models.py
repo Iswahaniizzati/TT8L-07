f4108f8948e0e415cce149af912f9fba9dc6d0fd
Nooratierah <nooratierahsyafiqah@gmail.com>
2024-06-10 02:22:11 +0800

    created cart field profile model

--- wisebuyer/store/models.py
+++ wisebuyer/store/models.py
@@ -14,6 +14,7 @@ class Profile(models.Model):
     postcode = models.CharField(max_length=20, blank=True)
     city = models.CharField(max_length=20, blank=True)
     state = models.CharField(max_length=20, blank=True)
+    old_cart = models.CharField(max_length=20, blank=True, null=True)
     
     def __str__(self):
         return self.user.username
