fcecc987ec126f8c51619a684de8121c7089e6a1
Kyra <kyraafia.fci@gmail.com>
2024-06-10 17:59:14 +0800

    get summary quantities and totals

--- wisebuyer/payment/views.py
+++ wisebuyer/payment/views.py
@@ -6,6 +6,8 @@ def checkout(request):
     # Get the cart
     cart = Cart(request)
     cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
     return render (request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
 
 
