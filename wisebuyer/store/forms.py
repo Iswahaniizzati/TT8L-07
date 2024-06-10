49df643e8ec16471ff7e7714cfb7567077b48ecb
Nur Iswahani <iswahani.izzati@gmail.com>
2024-06-10 22:25:09 +0800

    Update user profile

--- wisebuyer/store/forms.py
+++ wisebuyer/store/forms.py
@@ -1,7 +1,25 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from .models import Profile
+
+class UpdateUserForm(UserChangeForm):
+	#Hide Password stuff
+	password = None 
+	#Get other fields
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+
+	class Meta:
+		model = User
+		fields = ('username', 'email')
+
+	def __init__(self, *args, **kwargs):
+		super(UpdateUserForm, self).__init__(*args, **kwargs)
+
+		self.fields['username'].widget.attrs['class'] = 'form-control'
+		self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+		self.fields['username'].label = ''
+		self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
 
 class UserInfoForm(forms.ModelForm):
     phone = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone'}), required=False)
--- wisebuyer/store/views.py
+++ wisebuyer/store/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UserInfoForm
+from .forms import SignUpForm, UserInfoForm ,UpdateUserForm
 from payment.forms import ShippingForm
 from payment.models import ShippingAddress
 from django import forms
@@ -78,7 +78,20 @@ def product(request,pk):
 
 
 def update_user(request):
-    return render(request, 'update_user.html', {})
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request, "User Has Been Updated")
+            return redirect('home')
+        return render(request, "update_user.html",{'user_form':user_form})
+    else: 
+        messages.error(request, "You Must Be Logged In to Access That Page")
+        return redirect('home')
 
 def home(request):
     products = Product.objects.all()
