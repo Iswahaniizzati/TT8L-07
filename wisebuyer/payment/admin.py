3fdcfc2afe3583ffedf5a3441ef3f053307aa3d4
Kyra <kyraafia.fci@gmail.com>
2024-06-11 18:06:37 +0800

    create order item inline

--- wisebuyer/payment/admin.py
+++ wisebuyer/payment/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 
 # Register this model on admin
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+#Create an orderitem inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
